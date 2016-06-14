ca66e5b05270584bf2b66e9826533a782d1a5863
Michael Kalygin <michael.kalygin@gmail.com>
2016-06-14 08:26:24 +0300

    implement editor + styling

--- app/assets/javascripts/components/note-editor.jsx
+++ app/assets/javascripts/components/note-editor.jsx
@@ -26,7 +26,19 @@ export default class NoteEditor extends Component {
           {this.headerLinks}
         </Header>
         <div className="note-editor">
-          Hello!
+          <div className="editor">
+            <h4>EDITOR</h4>
+            <textarea placeholder="Write Markdown and LaTeX here..."></textarea>
+          </div>
+          <div className="preview">
+            <h4>PREVIEW</h4>
+            <div className="output">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          </div>
         </div>
       </div>
     );
